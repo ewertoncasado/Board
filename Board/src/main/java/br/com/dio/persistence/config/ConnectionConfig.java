3629b73d21c049459163b95ead751d8af7195e45
Ewerton Casado <43896587+ewertoncasado@users.noreply.github.com>
2025-03-10 00:13:37 +0000

    Update ConnectionConfig.java

--- Board/src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ Board/src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -13,8 +13,8 @@ public final class ConnectionConfig {
 
     public static Connection getConnection() throws SQLException {
         var url = "jdbc:mysql://localhost/board";
-        var user = "root";
-        var password = "C@s@d0@16$antos#";
+        var user = "board";
+        var password = "board";
         var connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
         return connection;
